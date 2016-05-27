ff26bdb87677877e386e19e38eb18a6ce9628fd4
Supasorn <supasorn@gmail.com>
2016-05-27 01:59:59 -0700

    auto commit

---
+++ delaunay_mesh.cc
@@ -0,0 +1,78 @@
+#include "delaunay_mesh.h"
+
+template <typename T>
+double DelaunayMesh::DistanceSquared(const T &p0, const T &p1) {
+  double v[3] = {p0.x() - p1.x(), p0.y() - p1.y(), p0.z() - p1.z()};
+  return v[0] * v[0] + v[1] * v[1] + v[2] * v[2];
+}
+
+// Specializing template functions for point with different CGAL kernels.
+template double DelaunayMesh::DistanceSquared(const DelaunayMesh::Delaunay::Point &p0, const DelaunayMesh::Delaunay::Point &p1);
+template double DelaunayMesh::DistanceSquared(const DelaunayMesh::TrianglePrimitive::Point &p0, const DelaunayMesh::TrianglePrimitive::Point &p1);
+
+
+void DelaunayMesh::AddPoint(const DelaunayMesh::DPoint &p, const DelaunayMesh::VertexInfo &vi) {
+  auto v = d.nearest_vertex(p);
+  // Found a nearest vertex within merge_threshold distance.
+  if (d.is_vertex(v) && DistanceSquared(p, v->point()) < merge_threshold) {
+    auto vp = v->point();
+    int n = v->info().n;
+    d.move(v, Delaunay::Point(
+          (p.x() + vp.x() * n) / (n + 1),
+          (p.y() + vp.y() * n) / (n + 1),
+          (p.z() + vp.z() * n) / (n + 1)));
+    v->info().n++;
+    // Merges camera list;
+    v->info().cams.insert(vi.cams.begin(), vi.cams.end());
+  } else {
+    auto v = d.insert(p);
+    v->info() = vi;
+  }
+
+}
+
+// Utility function to convert a custom point type to CGAL point type.
+DelaunayMesh::Point DelaunayMesh::TrianglePrimitive::convert(const DelaunayMesh::DPoint &p) const {
+  return DelaunayMesh::Point(p[0], p[1], p[2]);
+}
+    
+// On-the-fly conversion from the internal data to the CGAL types.
+DelaunayMesh::Datum DelaunayMesh::TrianglePrimitive::datum() const {
+  const auto &v0 = m_pt->first->vertex((m_pt->second + 1) % 4);
+  const auto &v1 = m_pt->first->vertex((m_pt->second + 2) % 4);
+  const auto &v2 = m_pt->first->vertex((m_pt->second + 3) % 4);
+  return Datum(convert(v0->point()), convert(v1->point()), convert(v2->point()));
+}
+
+// Returns a reference point which must be on the primitive.
+DelaunayMesh::Point DelaunayMesh::TrianglePrimitive::reference_point() const { 
+  const auto &p = m_pt->first->vertex((m_pt->second + 1) % 4)->point();
+  return DelaunayMesh::Point(p[0], p[1], p[2]);
+}
+
+// Builds AABB tree for fast ray intersection. Return the number of triangles.
+int DelaunayMesh::BuildAABBTree() {
+  triangles.clear();
+  tree.clear();
+  for (auto it = d.finite_facets_begin(); it != d.finite_facets_end(); it++) {
+    triangles.push_back(*it);
+  }
+  tree.insert(triangles.begin(),triangles.end());
+  return triangles.size();
+}
+
+void DelaunayMesh::RayIntersect(const DelaunayMesh::KSC::Ray_3 &ray, std::list<Tree::Intersection_and_primitive_id<KSC::Ray_3>::Type> &intersections) {
+  //tree.all_intersected_primitives(ray, std::back_inserter(primitives));
+  //std::list<Tree::Intersection_and_primitive_id<KSC::Ray_3>::Type> re;
+  tree.all_intersections(ray, std::back_inserter(intersections));
+
+}
+
+// Assigns IDs to all tetrahedrons after Delaunay is complete. Returns number of cells.
+int DelaunayMesh::AssignTetrahedronIds() {
+  int idCount = 0;
+  for (auto it = d.all_cells_begin(); it != d.all_cells_end(); it++) {
+    it->info().id = idCount++;
+  }
+  return idCount;
+}
---
+++ delaunay_mesh.h
@@ -0,0 +1,112 @@
+#ifndef MVSA_MESH_EXTRACTION_DELAUNAY_MESH_
+#define MVSA_MESH_EXTRACTION_DELAUNAY_MESH_
+
+#include <CGAL/AABB_traits.h>
+#include <CGAL/AABB_tree.h>
+#include <CGAL/AABB_triangle_primitive.h>
+#include <CGAL/Delaunay_triangulation_3.h>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/IO/Color.h>
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Triangulation_3.h>
+#include <CGAL/Triangulation_cell_base_with_info_3.h>
+#include <CGAL/Triangulation_vertex_base_with_info_3.h>
+#include <set>
+
+// This class is used to perform mesh extraction. The input is a set of point cloud.
+// Each point has a world-coordinate and a list of camera visible from that point.
+// The output is a visibility-consistent triangle mesh. 
+class DelaunayMesh {
+public:
+  // Vertex info class for used with Delaunay. This stores number of merged points
+  // and a list of camaera visible from this vertex.
+  struct VertexInfo {
+    std::set<int> cams;
+    int n;
+
+    VertexInfo() : n(1) {}
+    VertexInfo(int cam0, int cam1) : n(1) { 
+      cams.insert(cam0); 
+      cams.insert(cam1);
+    }
+  };
+
+  // Tetrahedron info class for used with Delaunay. It stores the id of this 
+  // tetrahedron.
+  struct TetraInfo {
+    int id;
+  };
+
+  // Could use Epick kernel, but Simple_cartesian is supposed to be fastest.
+  typedef CGAL::Simple_cartesian<double> KSC; 
+  typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+  typedef CGAL::Triangulation_vertex_base_with_info_3<VertexInfo, K> Vb;
+  typedef CGAL::Triangulation_cell_base_with_info_3<TetraInfo, K> Cb;
+  typedef CGAL::Triangulation_data_structure_3<Vb, Cb> Tds;
+  typedef CGAL::Delaunay_triangulation_3<K, Tds> Delaunay;
+  typedef DelaunayMesh::Tds::Cell_handle Cell_handle;
+  typedef DelaunayMesh::Delaunay::Point   DPoint;
+  typedef Delaunay::Facet Facet;
+
+  typedef std::vector<Facet>::const_iterator Iterator;
+
+  // The following primitive provides the conversion facilities between
+  // the custom triangle and point types and the CGAL ones.
+  struct TrianglePrimitive {
+  typedef KSC::Point_3    Point; // CGAL 3D point type.
+  typedef KSC::Triangle_3 Datum; // CGAL 3D triangle type.
+  public:
+    // This is the type of data that the queries returns. 
+    typedef const Facet* Id;
+    // CGAL types returned.
+  private:
+    Id m_pt; // This is what the AABB tree stores internally.
+  public:
+    TrianglePrimitive() {} 
+
+    // The following constructor is the one that receives the iterators from the 
+    // iterator range given as input to the AABB_tree.
+    TrianglePrimitive(Iterator it) : m_pt(&(*it)) {}
+    const Id& id() const { return m_pt; }
+
+    // Utility function to convert a custom point type to CGAL point type.
+    Point convert(const DPoint &p) const;
+    
+    // On-the-fly conversion from the internal data to the CGAL types.
+    Datum datum() const; 
+
+    // Returns a reference point which must be on the primitive.
+    Point reference_point() const;
+  };
+
+  typedef CGAL::AABB_traits<KSC, TrianglePrimitive> My_AABB_traits;
+  typedef CGAL::AABB_tree<My_AABB_traits> Tree;
+  typedef Tree::Primitive_id Primitive_id;
+  typedef TrianglePrimitive::Point Point;
+  typedef TrianglePrimitive::Datum Datum;
+
+  // Adds depth point to the Delaunay.
+  void AddPoint(const Delaunay::Point &p, const VertexInfo &vi);
+
+  // Builds AABB tree for fast ray intersection. Return the number of triangles.
+  int BuildAABBTree();
+  void RayIntersect(const DelaunayMesh::KSC::Ray_3 &ray, std::list<Tree::Intersection_and_primitive_id<KSC::Ray_3>::Type> &intersections);
+  int AssignTetrahedronIds();
+
+  template <typename T>
+  static double DistanceSquared(const T &p0, const T &p1);
+
+  Delaunay d;
+  Tree tree;
+private:
+  // If a new vertex is closer than merge_threshold to an existing vertex, 
+  // merge the two vertices. Otherwise, add the new vertex into Delaunay.
+  // TODO(supasorn): make a setter or option for this.
+  double merge_threshold = 0.00001;
+
+  // Triangle in AABB. This vector MUST persist throughout AABB usage. 
+  std::vector<Facet> triangles;
+
+};
+
+#endif
